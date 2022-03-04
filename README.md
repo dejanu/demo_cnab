f6203f41974020637e150ddcc2873b38def596b0
deal <alexandru.dejanu@systematic.com>
2022-03-04 16:49:29 +0200

    wowow

--- README.md
+++ README.md
@@ -10,7 +10,29 @@ Every deplyment is a snowflake
 - [Terraform](/terraformstuff/tf.md)
 - [Porter](/porterstuff/porterstuff.md)
 
-### Cmds:
+## Bundles:
+
+### dependencies:
+
+[Docker mixin](https://porter.sh/examples/docker/)
+
 ```bash
-porter explain --tag dejanualex/aks:v0.1.4
+
+# porter mixins install NAME [flags]
+porter mixins install docker
+porter mixins list
+
+# the app
+docker run docker/whalesay cowsay MUCH WOW
+
+porter install --reference getporter/whalesay:v0.1.2 --allow-docker-host-access
+
+porter invoke whalesay --action=say --param 'msg=MUCH WOW!' --allow-docker-host-access
+
+porter uninstall --reference getporter/whalesay:v0.1.2 --allow-docker-host-access
+
 ```
+
+* Nice links:
+https://book.kubebuilder.io/
+https://opensource.com/article/17/7/how-linux-containers-evolved
--- les_steps.sh
+++ les_steps.sh
@@ -3,26 +3,15 @@
 # install azure cnab driver
 curl https://raw.githubusercontent.com/deislabs/cnab-azure-driver/main/install-in-azure-cloudshell.sh |/bin/bash
 
-# my bundle
-dejanualex/dejanu-aks:v0.1.0
 
 # how to use the bundle
 porter explain --tag cnabquickstarts.azurecr.io/porter/aks/bundle:latest
 
+# az aks get-credentials --resource-group <rg_name> --name <cluster_name>
+az aks get-credentials --resource-group demorg --name democluster
+
 # installing the bundle
 porter install --tag cnabquickstarts.azurecr.io/porter/aks/bundle:latest \
--c aks \
---param azure_location=eastus \
---param cluster_name=democluster \
---param kubernetes_version=1.20.13 \
---param node_count=2 \
---param node_vm_size=Standard_D2s_v3 \
---param porter-debug=false \
---param resource_group=demorg \
---param vm_set_type=VirtualMachineScaleSets \
--d azure
-
-porter install --tag dejanualex/dejanu-aks:v0.1.0 \
 -c aks \
 --param azure_location=eastus \
 --param cluster_name=democluster \
